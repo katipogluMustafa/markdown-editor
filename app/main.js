e3dffb3681c403a438740bfb9c6dc6ef4f23ff19
Mustafa Katipoğlu <mustafa.katipoglu@outlook.com>
2023-09-24 22:24:50 +0300

    redesign: add graceful browser window init

--- app/main.js
+++ app/main.js
@@ -3,19 +3,37 @@ const { app, BrowserWindow} = require('electron');
 let mainWindow = null;
 
 app.on('ready', ()=>{
-    mainWindow = new BrowserWindow({
+    mainWindow = get_default_hidden_browser_window();
+    load_index_page();
+    show_the_window_when_dom_is_ready();
+    
+    mainWindow.on('closed', ()=>{
+        mainWindow = null;
+    });
+});
+
+function get_default_hidden_browser_window()
+{
+    return new BrowserWindow({
         width: 1366,
         height: 768,
         webPreferences: {
             nodeIntegration: true,
             worldSafeExecuteJavascript: false,
             contextIsolation: false,
-        }
+        },
+        show: false,
     });
+}
 
+function load_index_page()
+{
     mainWindow.webContents.loadFile('app/index.html');
-});
+}
 
-app.on('closed', ()=>{
-    mainWindow = null;
-});
+function show_the_window_when_dom_is_ready()
+{
+    mainWindow.once('ready-to-show', ()=>{
+        mainWindow.show();
+    });
+}
