fbc6e23fffe91c38d63131b0218696a303fbeae0
Mustafa Katipoğlu <mustafa.katipoglu@outlook.com>
2023-12-09 22:56:59 +0300

    refactor: use existing func over duplicating

--- src/main/UserDialog.js
+++ src/main/UserDialog.js
@@ -1,5 +1,6 @@
 const { app, dialog} = require('electron');
 const fs = require('fs');
+const { readFile } = require('./MarkdownDocument');
 
 function getFileFromUser(callingWindow)
 {
@@ -21,13 +22,7 @@ function getFileFromUser(callingWindow)
     let file1 = files[0];
     try
     {
-        const fileContent = fs.readFileSync(file1).toString();
-
-        //console.log(file1);
-        //console.log(fileContent);
-
-        callingWindow.webContents.send('file-opened', file1, fileContent);
-        app.addRecentDocument(file1);
+        readFile(callingWindow, file1);
     }
     catch(error)
     {
