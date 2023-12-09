3a748b30ccd7d3ed0c27db7a62d22da28c83537d
Mustafa Katipoğlu <mustafa.katipoglu@outlook.com>
2023-12-09 23:18:03 +0300

    refactor: remove no longer needed return

--- src/main/MainProcessServices.js
+++ src/main/MainProcessServices.js
@@ -24,7 +24,7 @@ function registerMainProcessServices()
     });
     
     ipcMain.handle('createWindow', ()=>{
-        return createWindow();
+        createWindow();
     });
     
     ipcMain.handle('setWindowTitle', (event, title)=>{
