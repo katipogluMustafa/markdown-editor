44fe462c3989f3a704170ffcaf3d527457da9292
Mustafa Katipoğlu <mustafa.katipoglu@outlook.com>
2023-12-10 16:22:32 +0300

    feat: add show file in explorer and in default editor into the context menu

--- src/main/ContextMenu.js
+++ src/main/ContextMenu.js
@@ -2,6 +2,8 @@ const { Menu } = require('electron');
 
 const markdownContextMenu = Menu.buildFromTemplate([
     {label: 'Open File', click(item, focusedWindow){ focusedWindow.webContents.send('open-file'); }},
+    {label: 'Show File in Folder', click(item, focusedWindow) { focusedWindow.webContents.send('show-file'); }},
+    {label: 'Open in Default Editor', click(item, focusedWindow) { focusedWindow.webContents.send('open-in-default'); }},
     {type: 'separator'},
     {label: 'Cut', role: 'cut'},
     {label: 'Copy', role: 'copy'},
